5b98b7b8e6c606fb870676649a99c4e8d0efba6d
Shubham Uikey <shubhamuikeys521@gmail.com>
2026-07-07 18:32:10 +0530

    Day9 java Prectice1

---
+++ Day9/Prectice1.java
@@ -0,0 +1,13 @@
+
+
+public class Prectice1 {
+    public static void main(String[] args) {
+        int age = 17;
+        if (age>18) {
+            System.out.println("yes duod you can drive!");
+        }
+        else{
+            System.out.println("No doud not yet!");
+        }
+    }
+}
